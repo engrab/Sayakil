b68728c607e2903a47bec458053aa15825d5693a
engrab <engrbilawalhussain@gmail.com>
2021-03-23 08:00:04 +0500

    sharef pref

---
+++ app/src/main/java/com/oman/sayakil/pref/SharedPreferencesManager.java
@@ -0,0 +1,161 @@
+package com.oman.sayakil.pref;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+
+
+public class SharedPreferencesManager {
+
+    private static final String APP_PREFS = "AppPrefsFile";
+    private static final String FIRST_NAME_KEY = "firstName";
+    private static final String LAST_NAME_KEY = "lastName";
+    private static final String DATE_BIRTH_KEY = "dateBirth";
+    private static final String PHONE_NUMBER_KEY = "phoneNumber";
+    private static final String SECOND_PHONE_NUMBER_KEY = "secndphoneNumber";
+    private static final String EMAIL_KEY = "email";
+    private static final String CONFORM_EMAIL_KEY = "conformemail";
+    private static final String L1_KEY = "l1Address";
+    private static final String L2_KEY = "l2Address";
+    private static final String ZIPCODE_KEY = "zipcode";
+    private static final String PROVINCCE_KEY = "province";
+    private static final String GENDER_KEY = "gender";
+
+
+    private SharedPreferences sharedPrefs;
+    private static SharedPreferencesManager instance;
+
+
+    private SharedPreferencesManager(Context context) {
+        sharedPrefs = context.getApplicationContext().getSharedPreferences(APP_PREFS, Context.MODE_PRIVATE);
+    }
+
+
+    public static synchronized SharedPreferencesManager getInstance(Context context) {
+
+        if (instance == null)
+            instance = new SharedPreferencesManager(context);
+
+        return instance;
+    }
+
+    public void setFirstName(String firstName) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(FIRST_NAME_KEY, firstName);
+        editor.apply();
+    }
+
+    public String getFirstName() {
+        return sharedPrefs.getString(FIRST_NAME_KEY, "name");
+    }
+
+    public void setLastName(String lastName) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(LAST_NAME_KEY, lastName);
+        editor.apply();
+    }
+
+    public String getLastName() {
+        return sharedPrefs.getString(LAST_NAME_KEY, "last");
+    }
+
+    public void setDateBirth(String dateBirth) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(DATE_BIRTH_KEY, dateBirth);
+        editor.apply();
+    }
+
+    public String getDateBirth() {
+        return sharedPrefs.getString(DATE_BIRTH_KEY, "12/23");
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(PHONE_NUMBER_KEY, phoneNumber);
+        editor.apply();
+    }
+
+    public String getPhoneNumber() {
+        return sharedPrefs.getString(PHONE_NUMBER_KEY, "123");
+    }
+
+    public void setSecondPhoneNumber(String phoneNumber) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(SECOND_PHONE_NUMBER_KEY, phoneNumber);
+        editor.apply();
+    }
+
+    public String getSecondPhoneNumber() {
+        return sharedPrefs.getString(SECOND_PHONE_NUMBER_KEY, "123");
+    }
+
+    public void setEmail(String email) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(EMAIL_KEY, email);
+        editor.apply();
+    }
+
+    public String getEmail() {
+        return sharedPrefs.getString(EMAIL_KEY, "abc@gmail.com");
+    }
+
+    public void setConformEmail(String conformEmail) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(CONFORM_EMAIL_KEY, conformEmail);
+        editor.apply();
+    }
+
+    public String getConformEmail() {
+        return sharedPrefs.getString(CONFORM_EMAIL_KEY, "abc@gmail.com");
+    }
+
+    public void setL1(String l1) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(L1_KEY, l1);
+        editor.apply();
+    }
+
+    public String getL1() {
+        return sharedPrefs.getString(L1_KEY, "this is primary address");
+    }
+
+    public void setL2(String phoneNumber) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(L2_KEY, phoneNumber);
+        editor.apply();
+    }
+
+    public String getL2() {
+        return sharedPrefs.getString(L2_KEY, "this is secondary address");
+    }
+
+    public void setZipcode(String  zipcode) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(ZIPCODE_KEY, zipcode);
+        editor.apply();
+    }
+
+    public String getZipcode() {
+        return sharedPrefs.getString(ZIPCODE_KEY, "0");
+    }
+
+    public void setProvincce(String provincce) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(PROVINCCE_KEY, provincce);
+        editor.apply();
+    }
+
+    public String getProvince() {
+        return sharedPrefs.getString(PROVINCCE_KEY, "ABC");
+    }
+
+    public void setGender(int position) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putInt(GENDER_KEY, position);
+        editor.apply();
+    }
+
+    public int getGender() {
+        return sharedPrefs.getInt(GENDER_KEY, 1);
+    }
+}
--- app/src/main/java/com/oman/sayakil/ui/activities/ProfileActivity.java
+++ app/src/main/java/com/oman/sayakil/ui/activities/ProfileActivity.java
@@ -1,17 +1,31 @@
 package com.oman.sayakil.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.oman.sayakil.R;
 import com.oman.sayakil.databinding.ActivityProfileBinding;
+import com.oman.sayakil.pref.SharedPreferencesManager;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
+    Calendar myCalendar;
+    ArrayAdapter<CharSequence> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +34,91 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(view);
         setSupportActionBar(binding.toolbar);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        getPref();
+
+        adapter = ArrayAdapter.createFromResource(
                 this, R.array.gender_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.spGender.setAdapter(adapter);
 
+
+
+        myCalendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+        binding.etDateBirth.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(ProfileActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        binding.etDateBirth.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.menu_save){
+            setPref();
+            Toast.makeText(this, "Save Information Successfully", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setPref() {
+        SharedPreferencesManager.getInstance(this).setFirstName(binding.etFirstName.getText().toString());
+        SharedPreferencesManager.getInstance(this).setLastName(binding.etLastName.getText().toString());
+        SharedPreferencesManager.getInstance(this).setDateBirth(binding.etDateBirth.getText().toString());
+        SharedPreferencesManager.getInstance(this).setPhoneNumber(binding.etPhoneNumber.getText().toString());
+        SharedPreferencesManager.getInstance(this).setSecondPhoneNumber(binding.etSecondPhoneNumber.getText().toString());
+        SharedPreferencesManager.getInstance(this).setEmail(binding.etEmail.getText().toString());
+        SharedPreferencesManager.getInstance(this).setConformEmail(binding.etConformEmail.getText().toString());
+        SharedPreferencesManager.getInstance(this).setL1(binding.etLine1.getText().toString());
+        SharedPreferencesManager.getInstance(this).setL2(binding.etLine2.getText().toString());
+        SharedPreferencesManager.getInstance(this).setZipcode(binding.etZip.getText().toString());
+        SharedPreferencesManager.getInstance(this).setProvincce(binding.etProvince.getText().toString());
+        SharedPreferencesManager.getInstance(this).setGender(binding.spGender.getSelectedItemPosition());
+
+    }
+
+    private void getPref() {
+        binding.etFirstName.setText(SharedPreferencesManager.getInstance(this).getFirstName());
+        binding.etLastName.setText(SharedPreferencesManager.getInstance(this).getLastName());
+        binding.etDateBirth.setText(SharedPreferencesManager.getInstance(this).getDateBirth());
+        binding.etPhoneNumber.setText(SharedPreferencesManager.getInstance(this).getPhoneNumber());
+        binding.etSecondPhoneNumber.setText(SharedPreferencesManager.getInstance(this).getSecondPhoneNumber());
+        binding.etEmail.setText(SharedPreferencesManager.getInstance(this).getEmail());
+        binding.etConformEmail.setText(SharedPreferencesManager.getInstance(this).getConformEmail());
+        binding.etLine1.setText(SharedPreferencesManager.getInstance(this).getL1());
+        binding.etLine2.setText(SharedPreferencesManager.getInstance(this).getL2());
+        binding.etZip.setText(SharedPreferencesManager.getInstance(this).getZipcode());
+        binding.etProvince.setText(SharedPreferencesManager.getInstance(this).getProvince());
+        binding.spGender.setSelection(SharedPreferencesManager.getInstance(this).getGender());
+        }
 }
