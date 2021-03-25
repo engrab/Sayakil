884dc5ebbc6d4f0f28c6b24f6c89347ab577bb90
engrab <engrbilawalhussain@gmail.com>
2021-03-25 21:56:41 +0500

    Message Fragment work

--- app/src/main/java/com/oman/sayakil/ui/drawer_fragments/MessageFragment.java
+++ app/src/main/java/com/oman/sayakil/ui/drawer_fragments/MessageFragment.java
@@ -1,14 +1,20 @@
 package com.oman.sayakil.ui.drawer_fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,6 +22,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.oman.sayakil.R;
 import com.oman.sayakil.databinding.FragmentMessageBinding;
+import com.oman.sayakil.databinding.ItemMessageBinding;
+import com.oman.sayakil.model.MessageModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,6 +46,7 @@ public class MessageFragment extends Fragment {
     private String mParam2;
 
     private FragmentMessageBinding binding;
+    private List<MessageModel> mList;
 
     public MessageFragment() {
         // Required empty public constructor
@@ -75,23 +87,69 @@ public class MessageFragment extends Fragment {
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("messages").child("msg1");
+        mList = new ArrayList<>();
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                binding.tvMsg.setText(value);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
 
+                mList.add(new MessageModel(value));
+                startRecyclerViewAdapter();
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
         return view;
+    }
+
+    private void startRecyclerViewAdapter() {
+        binding.rvMessage.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rvMessage.setAdapter(new MessageAdapter(getContext(), mList));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+
+    private class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+        private Context context;
+        private List<MessageModel> messageList;
+
+        public MessageAdapter(Context context, List<MessageModel> messageList) {
+            this.context = context;
+            this.messageList = messageList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ViewHolder(ItemMessageBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+            holder.bindingmessage.tvMsg.setText(messageList.get(position).getMessage());
+        }
+
+        @Override
+        public int getItemCount() {
+            return messageList.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public ItemMessageBinding bindingmessage;
+            public ViewHolder(@NonNull ItemMessageBinding binding1) {
+                super(binding1.getRoot());
+                bindingmessage = binding1;
+            }
+        }
     }
 }
